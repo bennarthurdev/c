51caaac7d82b4bd3aff062c2efc4da18fe78be6a
bennarthurpy <87674915+bennarthurpy@users.noreply.github.com>
2022-06-18 02:54:30 -0300

    Quase lá
    O erro está apenas na saída do resultado, tente corrígi-lo!

--- mean_filter/main.c
+++ mean_filter/main.c
@@ -1,55 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-float * getArrayElements(int size , float arr[]){
-    int x , y;
+//read array function
+float * getArrayElements(int size , float arr[])
+{
+  /*Read an array and return
+   <float>   
+   */
+    int x;
     for(x = 0 ; x < size ; x++){
+        printf("Element [%d]: " , x);
         scanf("%f" , &arr[x]);
     }
     return arr;
 }
 
-float * calculateAverage(int order , int size , float arr2[]){
-    int j = order, i; float sum[order] , avg[order];
-    
-        //avg[i] = arr2[j] + arr2[j+1] + arr2[order+2] ... + [order+order]/order; i++
-        for (i = order; i < (size/order) ; i++)
-        {
-            for (j = order; (order + order); j++)
-            {
-                sum[i] += arr2[j];
-            }
-            avg[i] = sum[i]/order;
-        }
-   
-    return avg;
+float totalValueOfInterval(int firstIndexReference ,int lastIndexReference , float arr[]){
+  int y; float sum;
+  for(y=firstIndexReference;y<=lastIndexReference;y++){
+    sum += arr[y];
+  }
+  return sum;
 }
-
-int main()
-{
-   int size = 15 , 
-   order ,
-   i //Change size value to manipulate the size of array
-   ;
-   float array[size];
-   //get data from array
-   getArrayElements(size , array);
-   
-   //get a order
-   printf("Type a order: ");
-   scanf("%d", &order);
-  
-   //CalculateAvarege
-   float *avarage;
-   avarage = calculateAverage(order , size , array);
-   //PrintAvarage
-   for (int i = 0; i <= size/order; i++)
-   {
-    printf("Media [%d] : %f" , i , *avarage);
-   }
-
-   return 0;
+float avarageOfSum(float sumInterval, int intervalSize){
+  /*receive a sum and dived by order reference and return the avarage in this interval*/
+  float avarage;
+  avarage = sumInterval/intervalSize;
+  return avarage;
 }
+//debbuging functions
+void printArray(int sizeArray , float arr[]){
+  for(int i = 0 ; i < sizeArray; i++){
+    printf("Array[%d] : %.2f\n " , i , arr[i]);
+  }
+}
+main(){
+  //define inicial array
+  int size, order , interval_size;//size of user array can be defined by own user
+  printf("Inicial Array Size: <integer>: \n");
+  scanf("%d", &size);
+  //scan incial array elements
+  float userArray[size];
+  getArrayElements(size , userArray);
+  //scan order
+  printf("Please choose a order \n:");
+  scanf("%d", &order);
+  //display array for debbuging
+ 
 
+  interval_size = (size/order);
 
+  //now we need a loop to generate a array of avarages
+  float avg[order], sum[order];//this array contain the avgs based on order chosed by user
+  int first = 0 , last = first + interval_size;
+  for(int i = 0; i < order ; i++){
+    sum[i] += totalValueOfInterval(first , last , userArray);
+    avg[i] = avarageOfSum(sum[i] , order);
+    first += interval_size;
+    last += interval_size; 
+  }
+    printArray(2, avg);
+
+  return 0;
+}
